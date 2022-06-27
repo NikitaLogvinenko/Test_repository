fd2361aa6daa4fa0ea31279e6a79747a74a92ece
NikitaLogvinenko <nichnikich@gmail.com>
2022-06-27 18:21:14 +0700

    explore merge conflicts, 1

--- first_file.py
+++ first_file.py
@@ -6,3 +6,5 @@ def int_summator(a, b):
 if __name__ == '__main__':
     print(eval('int_summator(1, 2)'))
     print('Summation successfully complete!')  # closing phrase
+
+    # merge conflict, v1
