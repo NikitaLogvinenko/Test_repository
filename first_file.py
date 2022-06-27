870ca9d6780cd2ad56a5c54f0eb364422ff2c66f
NikitaLogvinenko <nichnikich@gmail.com>
2022-06-27 18:12:05 +0700

    Add an annotation in the first_file.py and edit this file according to PEP8 (Ctrl+Alt+L)

--- first_file.py
+++ first_file.py
@@ -2,6 +2,7 @@ def int_summator(a, b):
     if isinstance(a, int) and isinstance(b, int):
         return a + b
 
+
 if __name__ == '__main__':
     print(eval('int_summator(1, 2)'))
-    print('Summation successfully complete!')
+    print('Summation successfully complete!')  # closing phrase
